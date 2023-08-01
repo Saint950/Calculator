a8fe1d8331314df9863f2ab19a3edcc078155e2f
Saint950 <tomideadegunju67@gmail.com>
2023-07-31 22:43:42 -0400

    Completed

--- CalculatorBox.java
+++ CalculatorBox.java
@@ -2,73 +2,88 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 
 public class CalculatorBox extends JFrame implements ActionListener {
 
-    JPanel control;
-    JPanel row1;
-    JPanel row2;
-    JPanel row3;
-    JPanel row4;
-    JPanel row5;
+        Double a;
+        String theResult;
 
-    JPanel showInput;
-    JTextField textField;
+        ArrayList<Double> noEntered = new ArrayList<>();
 
-    JButton add = new JButton("+");
-    JButton clear = new JButton("AC");
-    JButton delete = new JButton("D");
-    JButton subtract = new JButton("-");
-    JButton multiply = new JButton("*");
-    JButton divide = new JButton("/");
-    JButton egualTo = new JButton("=");
-    JButton dot = new JButton(".");
+        String operator = "null";
+
+        JPanel control;
+        JPanel row1;
+        JPanel row2;
+        JPanel row3;
+        JPanel row4;
+        JPanel row5;
+
+        JPanel showInput;
+        JTextField textField;
+        JPanel answerPanel;
+        JLabel answer;
+
+        JButton add = new JButton("+");
+        JButton clear = new JButton("AC");
+        JButton delete = new JButton("D");
+        JButton subtract = new JButton("-");
+        JButton multiply = new JButton("*");
+        JButton divide = new JButton("/");
+        JButton equalTo = new JButton("=");
+        //JButton dot = new JButton(".");
 
 
-    JButton[] numbers = new JButton[10];
-    String[] name = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        JButton[] numbers = new JButton[11];
+        String[] name = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."};
 
-    CalculatorBox(){
+        CalculatorBox() {
+
         clear.setFocusable(false);
-        clear.setFont(new Font("serif",Font.ITALIC,30));
+        clear.setFont(new Font("serif", Font.ITALIC, 30));
         clear.setBackground(Color.black);
         clear.setOpaque(true);
+        clear.addActionListener(this);
         clear.setBorderPainted(false);
         clear.setForeground(Color.orange);
 
         delete.setFocusable(false);
-        delete.setFont(new Font("serif",Font.PLAIN,30));
+        delete.addActionListener(this);
+        delete.setFont(new Font("serif", Font.PLAIN, 30));
 
         add.setFocusable(false);
-        add.setFont(new Font("serif",Font.PLAIN,30));
+        add.addActionListener(this);
+        add.setFont(new Font("serif", Font.PLAIN, 30));
 
         subtract.setFocusable(false);
-        subtract.setFont(new Font("serif",Font.PLAIN,30));
+        subtract.addActionListener(this);
+        subtract.setFont(new Font("serif", Font.PLAIN, 30));
 
         multiply.setFocusable(false);
-        multiply.setFont(new Font("serif",Font.PLAIN,30));
+        multiply.addActionListener(this);
+        multiply.setFont(new Font("serif", Font.PLAIN, 30));
 
         divide.setFocusable(false);
-        divide.setFont(new Font("serif",Font.PLAIN,30));
+        divide.addActionListener(this);
+        divide.setFont(new Font("serif", Font.PLAIN, 30));
 
-        dot.setFocusable(false);
-        dot.setFont(new Font("serif",Font.PLAIN,30));
-
-        egualTo.setFocusable(false);
-        egualTo.setFont(new Font("serif",Font.PLAIN,30));
-
+        equalTo.setFocusable(false);
+        equalTo.addActionListener(this);
+        equalTo.setFont(new Font("serif", Font.PLAIN, 30));
 
         control = new JPanel();
-        control.setPreferredSize(new Dimension(600,480));
+        control.setPreferredSize(new Dimension(600, 480));
         control.setBackground(Color.darkGray);
-        control.setLayout(new GridLayout(5,1,0,0));
+        control.setLayout(new GridLayout(5, 1, 0, 0));
 
         row1 = new JPanel();
-        row1.setLayout(new GridLayout(1,4));
+        row1.setLayout(new GridLayout(1, 4));
         row1.setBackground(Color.gray);
 
         row2 = new JPanel();
-        row2.setLayout(new GridLayout(1,4));
+        row2.setLayout(new GridLayout(1, 4));
         row2.setBackground(Color.gray);
 
         row3 = new JPanel();
@@ -87,7 +102,7 @@ public class CalculatorBox extends JFrame implements ActionListener {
             numbers[i] = new JButton(name[i]);
             numbers[i].addActionListener(this);
             numbers[i].setFocusable(false);
-            numbers[i].setFont(new Font("serif",Font.PLAIN,30));
+            numbers[i].setFont(new Font("serif", Font.PLAIN, 30));
             numbers[i].setForeground(Color.black);
             //control.add(numbers[i]);
 
@@ -108,9 +123,9 @@ public class CalculatorBox extends JFrame implements ActionListener {
         row4.add(numbers[2]);
         row4.add(numbers[3]);
         row4.add(add);
-        row5.add(dot);
+        row5.add(numbers[10]);
         row5.add(numbers[0]);
-        row5.add(egualTo);
+        row5.add(equalTo);
         row5.add(divide);
 
         control.add(row1);
@@ -120,14 +135,27 @@ public class CalculatorBox extends JFrame implements ActionListener {
         control.add(row5);
 
         showInput = new JPanel();
-        showInput.setPreferredSize(new Dimension(650,120));
-        showInput.setBackground(new Color(190,150,0));
-        showInput.setLayout(new GridLayout(1,1));
+        showInput.setPreferredSize(new Dimension(650, 120));
+        showInput.setBackground(new Color(190, 150, 0));
+        showInput.setLayout(new BorderLayout());
 
-        textField = new JTextField(1);
-        textField.setFont(new Font("serif",Font.PLAIN,35));
+        textField = new JTextField("");
+        textField.setFont(new Font("serif", Font.PLAIN, 50));
         textField.setForeground(Color.black);
-        showInput.add(textField);
+
+        answerPanel = new JPanel();
+        answerPanel.setLayout(new BorderLayout());
+        answerPanel.setBackground(new Color(190, 150, 0));
+
+        answer = new JLabel("");
+        answer.setFont(new Font("serif", Font.PLAIN, 50));
+        //answer.setHorizontalTextPosition(600);
+        answer.setForeground(Color.black);
+
+        answerPanel.add(answer, BorderLayout.EAST);
+
+        showInput.add(textField, BorderLayout.NORTH);
+        showInput.add(answerPanel, BorderLayout.SOUTH);
 
 
         this.setTitle("INCH CALCULATOR");
@@ -144,7 +172,243 @@ public class CalculatorBox extends JFrame implements ActionListener {
 
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed (ActionEvent e){
+
+        for (JButton btn : numbers) {
+            JButton selectedBtn = (JButton) e.getSource();
+            if (selectedBtn == btn) {
+                add.setEnabled(true);
+                subtract.setEnabled(true);
+                divide.setEnabled(true);
+                multiply.setEnabled(true);
+                appendToOutput(btn.getText());
+            }
+        }
+
+        if (e.getSource() == delete) {
+            int len = textField.getText().length();
+            textField.setText(textField.getText().substring(0, len - 1));
+        }
+
+        else if (e.getSource() == clear) {
+            noEntered.clear();
+            textField.setText("");
+            answer.setText("");
+            operator = "null";
+        }
+
+        else if (e.getSource() == add) {
+            if (operator.equalsIgnoreCase("add") || operator.equalsIgnoreCase("null")) {
+                operator = "add";
+                try {
+                    a = Double.parseDouble(textField.getText());
+                } catch (NumberFormatException q) {
+                    System.out.print(".");
+                }
+                noEntered.add(a);
+                textField.setText("");
+                if (noEntered.size() < 2) {
+                    noEntered.add(0.0);
+                }
+                Calculate(noEntered);
+            }
+            else if (!operator.equalsIgnoreCase("add")){
+                a = Double.parseDouble(textField.getText());
+                noEntered.add(a);
+                textField.setText("");
+                Calculate(noEntered);
+            }
+            operator = "add";
+        }
+
+        else if (e.getSource() == subtract) {
+            if (operator.equalsIgnoreCase("-") || operator.equalsIgnoreCase("null")) {
+                operator = "-";
+                try {
+                    a = Double.parseDouble(textField.getText());
+                } catch (NumberFormatException q) {
+                    System.out.print(".");
+                }
+                noEntered.add(a);
+                textField.setText("");
+                if (noEntered.size() < 2) {
+                    noEntered.add(0.0);
+                }
+                Calculate(noEntered);
+            }
+            else if (!operator.equalsIgnoreCase("-")){
+                a = Double.parseDouble(textField.getText());
+                noEntered.add(a);
+                textField.setText("");
+                Calculate(noEntered);
+            }
+            operator = "-";
+        }
+
+        else if (e.getSource() == multiply) {
+            if (operator.equalsIgnoreCase("*") || operator.equalsIgnoreCase("null")) {
+                operator = "*";
+                try {
+                    a = Double.parseDouble(textField.getText());
+                } catch (NumberFormatException q) {
+                    System.out.print(".");
+                }
+                noEntered.add(a);
+                textField.setText("");
+                if (noEntered.size() < 2) {
+                    noEntered.add(1.0);
+                }
+                Calculate(noEntered);
+            }
+            else if (!operator.equalsIgnoreCase("*")){
+                a = Double.parseDouble(textField.getText());
+                noEntered.add(a);
+                textField.setText("");
+                Calculate(noEntered);
+            }
+            operator = "*";
+        }
+
+        else if (e.getSource() == divide) {
+            if (operator.equalsIgnoreCase("/") || operator.equalsIgnoreCase("null")) {
+                operator = "/";
+                try {
+                    a = Double.parseDouble(textField.getText());
+                } catch (NumberFormatException q) {
+                    System.out.print(".");
+                }
+                noEntered.add(a);
+                textField.setText("");
+                if (noEntered.size() < 2) {
+                    noEntered.add(1.0);
+                }
+                Calculate(noEntered);
+            }
+            else if (!operator.equalsIgnoreCase("/")){
+                a = Double.parseDouble(textField.getText());
+                noEntered.add(a);
+                textField.setText("");
+                Calculate(noEntered);
+            }
+            operator = "/";
+        }
+
+        else if (e.getSource() == equalTo) {
+            try {
+                a = Double.parseDouble(textField.getText());
+            } catch (NumberFormatException q){
+                System.out.print("");
+            }
+            noEntered.add(a);
+            textField.setText("");
+            if (noEntered.size() < 2) {
+                noEntered.add(0.0);
+            }
+            Calculate(noEntered);
+            noEntered.clear();
+            add.setEnabled(false);
+            subtract.setEnabled(false);
+            divide.setEnabled(false);
+            multiply.setEnabled(false);
+            operator = "null";
+        } else {
+            //System.out.println(e.getSource());
+        }
+    }
+
+
+
+    private void appendToOutput (String text){
+
+        String current = textField.getText();
+        String result = current + text;
+
+        textField.setText(result);
+    }
+
+
+    private void Calculate (ArrayList<Double> arrayList){
+
+        if (operator.equalsIgnoreCase("add")) {
+            double c = arrayList.get(0);
+            //System.out.println(c);
+            double d = arrayList.get(1);
+
+            double e = c + d;
+            arrayList.set(0, e);
+            arrayList.remove(1);
+
+            double y = arrayList.get(0);
+            theResult = Double.toString(y);
+
+            // textField.setText(theResult);
+
+            answer.setText(process(theResult));
+            // System.out.println(theResult);
+        } else if (operator.equalsIgnoreCase("-")) {
+            double c = arrayList.get(0);
+            double d = arrayList.get(1);
+
+            double e = c - d;
+            arrayList.set(0, e);
+            arrayList.remove(1);
+
+            double y = arrayList.get(0);
+            theResult = Double.toString(y);
+
+            // textField.setText(theResult);
+
+            answer.setText(process(theResult));
+            // System.out.println(theResult);
+        } else if (operator.equalsIgnoreCase("*")) {
+            double c = arrayList.get(0);
+            double d = arrayList.get(1);
+
+            double e = c * d;
+            arrayList.set(0, e);
+            arrayList.remove(1);
+
+            double y = arrayList.get(0);
+            theResult = Double.toString(y);
+
+            // textField.setText(theResult);
+
+            answer.setText(process(theResult));
+            // System.out.println(theResult);
+        } else if (operator.equalsIgnoreCase("/")) {
+            double c = arrayList.get(0);
+            double d = arrayList.get(1);
+
+            double e = c / d;
+            arrayList.set(0, e);
+            arrayList.remove(1);
+
+            double y = arrayList.get(0);
+            theResult = Double.toString(y);
+
+            // textField.setText(theResult);
+
+            answer.setText(process(theResult));
+            // System.out.println(theResult);
+        }
+        else {
+            System.out.println("unexpected error");
+        }
 
     }
-}
+
+
+    public String process (String theResult){
+        if (theResult.length() > 0) {
+            String integerPart = theResult.split("\\.")[0];
+            String decimalPart = theResult.split("\\.")[1];
+            if (decimalPart.equals("0")) {
+                theResult = integerPart;
+
+            }
+        }
+        return theResult;
+    }
+
+
+    }
